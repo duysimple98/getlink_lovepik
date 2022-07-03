f01d9e0b829265c34268b2f09b4d676c6fc339a9
Duy Do <sieuanhhung98@gmail.com>
2022-07-03 23:18:12 +0700

    Add option two button

--- getlink/views.py
+++ getlink/views.py
@@ -1,4 +1,5 @@
 from ast import arg
+from lib2to3.pgen2.pgen import PgenGrammar
 from turtle import title
 from xmlrpc.client import Boolean, boolean
 from django.shortcuts import render
@@ -17,6 +18,7 @@ import time
 
 # Create your views here.
 
+
 @csrf_exempt
 def get_name(request):
     form = SearchForm()
@@ -26,7 +28,7 @@ def get_name(request):
             text = form['look'].value()
             form = SearchForm()
             username = "duysimple98@gmail.com"
-            password = "adminbkh123"
+            password = "Adminbkh123@"
             chrome_options = Options()
 
             # tách chrome
@@ -67,71 +69,127 @@ def get_name(request):
 
             driver.switch_to.window(driver.window_handles[0])
 
-
             try:
-                elem = driver.find_element(By.XPATH, '//i[@class="iconfont icon-guanbitubiao btn-close-login-sign"]')
+                elem = driver.find_element(
+                    By.XPATH, '//i[@class="iconfont icon-guanbitubiao btn-close-login-sign"]')
                 if elem.is_displayed():
                     elem.click()
                     print("Có phần tử xuất hiện")
             except NoSuchElementException:
                 print("Không tìm thấy phần tử")
 
+
             driver.execute_script("window.open('about:blank', 'secondtab');")
+            driver.switch_to.window(driver.window_handles[0])
+            driver.close()
             driver.switch_to.window("secondtab")
             driver.get(text)
+            print("Tab đầu tiên là: ", driver.current_url)
 
             time.sleep(2)
 
-            # driver.find_element(
-            #     By.XPATH, '//div[@class="down_box"]//*[@class="pub psd"]').click()
-
             try:
-                psd = driver.find_element(By.XPATH, '//div[@class="down_box"]//*[@class="pub psd"]')
-                # jpg = driver.find_element(By.XPATH, '//div[@class="down_box"]//*[@class="pub jpg copyDownTips"]')
+                psd = driver.find_element(
+                    By.XPATH, '//div[@class="down_box"]//*[@class="pub psd"]')
                 if psd.is_displayed():
                     psd.click()
                     print("Đây là file PSD")
             except NoSuchElementException:
                 try:
-                    jpg = driver.find_element(By.XPATH, '//div[@class="down_box"]//*[@class="pub jpg copyDownTips"]')
+                    jpg = driver.find_element(
+                        By.XPATH, '//div[@class="down_box"]//*[@class="pub jpg copyDownTips"]')
                     if jpg.is_displayed():
                         jpg.click()
                         print("Đây là file JPG")
                 except NoSuchElementException:
                     try:
-                        combo = driver.find_element(By.XPATH, '//div[@class="pub jpg_psd"]//*[@class="pub_s psd_s"]')
+                        combo = driver.find_element(
+                            By.XPATH, '//div[@class="pub jpg_psd"]')
                         if combo.is_displayed():
-                            combo.click()
-                            print("Đây là link 2 nút")
+                            id = text.split("image-", 1)[1].split("/")[0]
+                            print("ID của template là: ", id)
+                            driver.execute_script("window.open('about:blank', 'thirdtab');")
+                            driver.switch_to.window("thirdtab")
+                            # driver.switch_to.window(driver.window_handles[1])
+                            driver.get('https://lovepik.com/download/detail/' + id + '?byso=&type=3')
+                            print("Tab thứ 2 là: ",driver.current_url )
+                            try:
+                                close_download = driver.find_element(
+                                    By.XPATH, '//span[@class="btn_close act_lazy"]')
+                                if close_download.is_displayed():
+                                    close_download.click()
+                                    print("Có phần tử xuất hiện")
+                            except NoSuchElementException:
+                                print("Không tìm thấy phần tử")
+                            content_jpg = driver.find_element(By.XPATH, '//input[@class="copy-ipt link"]').get_attribute("value")
+
+                            print("Link download JPG là: ", content_jpg)
+                            driver.close()
+                            driver.switch_to.window(driver.window_handles[0])
+                            print("Tab hiện tại là: ", driver.current_url)
+                            time.sleep(2)
+                            psd_s = driver.find_element(By.XPATH, '//div[@class="pub jpg_psd"]//*[@class="pub_s psd_s"]')
+                            if psd_s.is_displayed():
+                                psd_s.click()
+                                print("Đã lấy thành công nút")
+                                print("Tab thứ 3 là: ", driver.current_url)
+                                try:
+                                    close_download = driver.find_element(
+                                        By.XPATH, '//span[@class="btn_close act_lazy"]')
+                                    if close_download.is_displayed():
+                                        close_download.click()
+                                        print("Ok có nút")
+                                except NoSuchElementException:
+                                    print("Chả thấy đâu cả")
+                                time.sleep(2)
+                                psd_content = driver.find_element(By.XPATH, '//input[@class="copy-ipt link"]').get_attribute("value")
+                                print("Link file PSD 2 nút là: ",psd_content)
+                            driver.quit()
+                            return render(request, "index.html", {'form': form, 'link': content_jpg, 'psd_content': psd_content})
+                            
                     except NoSuchElementException:
                         print("Không tìm thấy bất kỳ phần tử nào")
                         content = "Link bạn nhập sai yêu cầu. Vui lòng kiểm tra lại."
                         return render(request, "index.html", {'form': form, 'link': content})
-
+                        # try:
+                        #     video = driver.find_element(By.XPATH, '//div[@class="limit_down"]')
+                        #     if video.is_displayed():
+                        #         video_content = driver.find_element(By.XPATH, '//a[@class="down_t"]').get_attribute("value")
+                        #         print("Giá trị video là: ", video_content)
+                        # except NoSuchElementException:
+                        #     print("Không tìm thấy bất kỳ phần tử nào")
+                        #     content = "Link bạn nhập sai yêu cầu. Vui lòng kiểm tra lại."
+                        #     return render(request, "index.html", {'form': form, 'link': content})
             time.sleep(2)
 
-            driver.switch_to.window(driver.window_handles[1])
 
-            print(driver.current_url)
+            # driver.switch_to.window(driver.window_handles[1])
+
+            # print("Tab hiện tại là: ", driver.current_url)
+            
+            # try:
+            #     combo_psd = driver.find_element(By.XPATH, '//div[@class="pub jpg_psd"]')
+            #     if combo_psd.is_displayed():
+            #         psd_s = driver.find_element(By.XPATH, '//a[@class="pub_s psd_s"]').get_attribute("onclick")
+            #         print ("PSD 2 nút có link: ",psd_s)
+            #         driver.execute_script(psd_s)
+            # except NoSuchElementException:
+            #     print("Không tìm thấy bất kỳ phần tử nào")
+            #     content = "Link bạn nhập sai yêu cầu. Vui lòng kiểm tra lại."
+            #     return render(request, "index.html", {'form': form, 'link': content})
+
 
             try:
-                close_download = driver.find_element(By.XPATH, '//span[@class="btn_close act_lazy"]')
+                close_download = driver.find_element(
+                    By.XPATH, '//span[@class="btn_close act_lazy"]')
                 if close_download.is_displayed():
                     close_download.click()
                     print("Có phần tử xuất hiện")
             except NoSuchElementException:
                 print("Không tìm thấy phần tử")
 
-            # driver.find_element(
-            #     By.XPATH, '//span[@class="btn_close act_lazy"]').click()
 
             time.sleep(2)
-
-            # try:
-            #     s = driver.find_element(By.XPATH, '//span[@class="copy-span"]').click()
-            #     print("Copied")
-            # except:
-            #     print("Error Copy")
 
             content = driver.find_element(
                 By.XPATH, '//input[@class="copy-ipt link"]').get_attribute("value")
