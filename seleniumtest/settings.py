a92b4e96bf2f3209fdffab7e88660ac4581f4238
Duy Do <sieuanhhung98@gmail.com>
2022-07-04 09:34:08 +0700

    Heroku app

--- seleniumtest/settings.py
+++ seleniumtest/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["getlinklovepik.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["getlovepik.herokuapp.com", "localhost"]
 
 
 # Application definition
